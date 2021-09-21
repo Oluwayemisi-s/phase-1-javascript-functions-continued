2a38e17e3328d52ba72054b7bfed77ede1780962
Oluwayemisi Soremekun <oluwayemisi.soremekun@gmail.com>
2021-09-21 10:05:02 -0700

    commit tree 100849026c5620a295f78cbe6ce8ead8b54ce9d4

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(vFlair = "*"){
+
+       const innerFunction = function (x = 'special'){
+            return `You are ${vFlair}${x}${vFlair}!`
+        }
+        return innerFunction
+}
+
+console.log(wrapAdjective('%')('a great programmer'))
+//console.log(saturdayFun('swim and dive'))
+//mondayWork() 
